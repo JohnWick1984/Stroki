ee621f837979cfd2886878faef6fa6fc4696734c
Евгений <@EUGENE1984>
2023-09-30 13:06:04 +0500

    Добавьте файлы проекта.

---
+++ Stroki/Stroki.cpp
@@ -0,0 +1,33 @@
+﻿// Stroki.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+
+    string input;
+    cout << "Enter your line: ";
+    getline(cin, input);
+
+    istringstream stream(input);
+    string word, longestWord;
+    size_t maxLength = 0;
+
+    while (stream >> word)
+    {
+        if (word.length() > maxLength)
+        {
+            maxLength = word.length();
+            longestWord = word;
+        }
+    }
+
+    cout << "Longest word: " << longestWord << endl;
+    cout << "Length of longest word: " << maxLength << endl;
+
+    return 0;
+}
---
+++ polydrom/polydrom.cpp
@@ -0,0 +1,61 @@
+﻿// polydrom.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+bool isPalindrome(const string& str) 
+{
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right)
+    {
+        if (str[left] != str[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int countPalindromes(const string& input) 
+{
+    int count = 0;
+    vector<string> words;
+    string word;
+    istringstream stream(input);
+
+    while (stream >> word)
+    {
+        words.push_back(word);
+    }
+
+    for (const string& word : words)
+    {
+        if (isPalindrome(word)) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    string input;
+    cout << "Enter the line: ";
+    getline(cin, input);
+
+    int palindromeCount = countPalindromes(input);
+
+    cout << "Number of polidromes in line: " << palindromeCount << endl;
+
+    return 0;
+}
